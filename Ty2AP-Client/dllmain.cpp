93df19b663752dab8fff849d416b272a572d4f07
FyreDay <fyredaymc@gmail.com>
2025-05-11 03:42:26 -0500

    implement get object function

--- Ty2AP-Client/dllmain.cpp
+++ Ty2AP-Client/dllmain.cpp
@@ -3,6 +3,23 @@
 void TickBeforeGame(float deltaSeconds) {
 }
 
+bool WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_KEYDOWN && wParam == VK_F2) {
+        uintptr_t result;
+        int id = 3680;
+        uintptr_t(*getObjPtr)(int) = reinterpret_cast<uintptr_t(*)(int)>(((uintptr_t)API::Get()->param()->TyHModule) + 0x00314f10);
+        __asm{
+            push id
+            call getObjPtr
+            add esp, 0x4
+            mov result, eax
+        }
+        API::LogPluginMessage(std::to_string(result));
+        
+    }
+    return false;
+}
+
 
 extern "C" __declspec(dllexport) bool TygerFrameworkPluginInitialize(TygerFrameworkPluginInitializeParam* param) {
     //Make sure to call this first before any API Functions
@@ -10,6 +27,8 @@ extern "C" __declspec(dllexport) bool TygerFrameworkPluginInitialize(TygerFramew
         return false;
 
     API::AddTickBeforeGame(TickBeforeGame);
+    API::AddPluginWndProc((WndProcFunc)WndProc);
+    API::LogPluginMessage(std::to_string(((int)API::Get()->param()->TyHModule)));
     return true;
 }
 
